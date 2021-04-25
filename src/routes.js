cd9de8ab8859a5dd1041cd3cdfcce9f2880fa469
nofacez <genizzz@yandex.ru>
2021-04-25 10:33:47 +0300

    added new routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const host = '';
 const prefix = 'api/v1';
 
 export default {
+  loginPath: () => [host, prefix, 'login'].join('/'),
+  dataPath: () => [host, prefix, 'data'].join('/'),
   channelsPath: () => [host, prefix, 'channels'].join('/'),
   channelPath: (id) => [host, prefix, 'channels', id].join('/'),
   channelMessagesPath: (id) => [host, prefix, 'channels', id, 'messages'].join('/'),
