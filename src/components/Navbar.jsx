dbaa96f5e03fb3b06cb255272fc7c299eda6bb70
nofacez <64412368+nofacez@users.noreply.github.com>
2021-04-26 18:21:27 +0300

    moved navbar to a separete file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
-const Navbar = () => (
-  <nav className="navbar navbar-light bg-light">
-    <Link to="/" className="navbar-brand">Hexlet Chat</Link>
-  </nav>
-);
+const Navbar = () => {
+  const { t } = useTranslation();
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <Link to="/" className="navbar-brand">{ t('navbar.title') }</Link>
+    </nav>
+  );
+};
 
 export default Navbar;
