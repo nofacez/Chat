6232c7d6260443284fdac33bf6dded327aa05247
nofacez <64412368+nofacez@users.noreply.github.com>
2021-04-26 18:22:54 +0300

    added new text

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,10 +1,17 @@
 export default {
   translation: {
+    navbar: {
+      title: 'Hexlet Chat',
+    },
     login: {
       username: 'Ваш ник',
       password: 'Пароль',
       enterButton: 'Войти',
       wrongPassAndUsernameError: 'Неверные имя пользователя или пароль',
+    },
+    chat: {
+      channels: 'Каналы',
+      sendMsgBtn: 'Отправить',
     },
   },
 };
