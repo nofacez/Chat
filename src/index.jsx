a370ed6260e62b4d265dedc746d4069a6eaa6bb8
nofacez <64412368+nofacez@users.noreply.github.com>
2021-05-03 23:02:22 +0300

    added socket context provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
 // @ts-check
-
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 import React from 'react';
@@ -10,16 +9,18 @@ import { UserProvider } from './components/context/UserContext.jsx';
 import './i18n';
 import store from './app/store.js';
 import '../assets/application.scss';
+import SockenContext, { socket } from './components/context/SocketContext.js';
 
 // @ts-ignore
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
-
 render(
   <Provider store={store}>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <SockenContext.Provider value={socket}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </SockenContext.Provider>
   </Provider>, document.getElementById('chat'),
 );
