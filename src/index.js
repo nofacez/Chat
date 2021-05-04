277b52fba2e3e2ecf89c2133f32b1afdea86b4f9
nofacez <64412368+nofacez@users.noreply.github.com>
2021-05-04 10:50:12 +0300

    hexlet tests#2.3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import run from './index.jsx';
+// @ts-check
+
+import 'core-js/stable/index.js';
+import 'regenerator-runtime/runtime.js';
+import './i18n';
+
+import run from './init.jsx';
+import '../assets/application.scss';
 
 run();
--- src/init.jsx
+++ src/init.jsx
@@ -1,14 +1,14 @@
 // @ts-check
-import 'core-js/stable/index.js';
-import 'regenerator-runtime/runtime.js';
+// import 'core-js/stable/index.js';
+// import 'regenerator-runtime/runtime.js';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App.jsx';
 import { UserProvider } from './components/context/UserContext.jsx';
-import './i18n';
+// import './i18n';
 import store from './app/store.js';
-import '../assets/application.scss';
+// import '../assets/application.scss';
 import SocketContext, { socket } from './components/context/SocketContext.js';
 import RollbarContext, { rollbar } from './components/context/RollbarContext.js';
 
